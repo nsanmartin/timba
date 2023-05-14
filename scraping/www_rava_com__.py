d20deb3723442818cbaff56ed6e479c0ddbea19b
nico <smnicolas@gmail.com>
2023-05-14 17:20:22 -0300

    Create scraping

---
+++ scraping/www_rava_com__.py
@@ -0,0 +1,16 @@
+from src import soup
+import json
+
+def response_mapping_corizaciones_dolares(text):
+    assert text
+    page = soup.get_soup(text) 
+    assert page
+    main_layout = page.find("main-layout")
+    dolares_p = page.find("dolares-p")
+    assert dolares_p 
+    res = dolares_p.attrs[':datos']
+    assert res
+    resJson = json.loads(res)
+    assert(resJson)
+    return(resJson)
+
--- scripts/rav_artous.py
+++ scripts/rav_artous.py
@@ -3,24 +3,11 @@ import sys
 import pandas as pd
 import json
 from src import cache
-from src import soup
+from scraping.www_rava_com__ \
+    import response_mapping_corizaciones_dolares as response_mapping
 
 one_day = 60 * 60 * 24
 one_year = one_day * 365
-
-def response_mapping_rav_dolars(text):
-    assert text
-    page = soup.get_soup(text) 
-    assert page
-    main_layout = page.find("main-layout")
-    dolares_p = page.find("dolares-p")
-    assert dolares_p 
-    res = dolares_p.attrs[':datos']
-    assert res
-    resJson = json.loads(res)
-    assert(resJson)
-    return(resJson)
-
 
 
 def url_fetch_and_map(url, expiration, response_mapping):
@@ -42,7 +29,7 @@ if __name__ == "__main__":
     rava = url_fetch_and_map(
             'https://www.rava.com/cotizaciones/dolares',
             args.expiration,
-            response_mapping_rav_dolars
+            response_mapping
     )
 
     for r in args.rest:
